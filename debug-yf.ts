08d93ac59401a73f5c70a3eee1c7e4acaf162aff
acandia <candia.antonio@gmail.com>
2025-12-29 11:20:48 -0400

    chore: delete debug-yf.ts and tsconfig.tsbuildinfo, and add tsbuildinfo to gitignore.

--- debug-yf.ts
+++
@@ -1,9 +0,0 @@
-const yahooFinance = require('yahoo-finance2');
-
-console.log('Type of module:', typeof yahooFinance);
-console.log('Keys:', Object.keys(yahooFinance));
-if (yahooFinance.default) {
-    console.log('Type of default:', typeof yahooFinance.default);
-    console.log('Keys of default:', Object.keys(yahooFinance.default));
-    console.log('Is default a constructor?', typeof yahooFinance.default === 'function' && /^\s*class\s+/.test(yahooFinance.default.toString()));
-}
